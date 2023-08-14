cd38f889db1b521d8accb6e637dcea5f533cec83
Rami Yassin <ramiyass@hotmail.com>
2023-08-14 07:31:48 +0300

    my first commit

---
+++ CsharpBot/Pages/Index.cs
@@ -0,0 +1,53 @@
+﻿using CsharpBot.Model;
+using CsharpBot.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Runtime.Serialization.Formatters;
+
+namespace CsharpBot.Pages
+{
+    public partial class Index
+    {
+        private  string _userQuestion = string.Empty;
+        private readonly List<Message> _conversationHistory =new List<Message>();
+        bool isSendingMessage;
+        private async Task HandleKeyPress(KeyboardEventArgs e)
+        {
+            if (e.Key is not "Enter") return;
+            await SendMessage();
+        }
+
+        private async Task SendMessage()
+        {
+            if (string.IsNullOrWhiteSpace(_userQuestion)) return;
+            AddUserQuestionToConversation();
+            StateHasChanged();
+            await CreateCompletion();
+            ClearInput();
+            StateHasChanged();
+        }
+
+        private void ClearInput()
+       =>_userQuestion = string.Empty;
+
+        private async Task CreateCompletion()
+        {
+            isSendingMessage=true;
+            var assistantResponse= await OpenAIService.CreateChatCompletion(_conversationHistory);
+            _conversationHistory.Add(assistantResponse);
+            isSendingMessage=false;
+        }
+
+        private void AddUserQuestionToConversation()
+        =>_conversationHistory.Add(new Message {role="user",content=_userQuestion});
+        private void ClearConservation()
+        {
+            ClearInput();
+            _conversationHistory.Clear();
+        }
+        [Inject]
+        public OpenAIService OpenAIService { get; set; }
+        public List<Message> Messages => _conversationHistory.Where(c=>c.role is not "system ").ToList();
+        
+    }
+}
---
+++ CsharpBot/Services/OpenAIService.cs
@@ -0,0 +1,26 @@
+﻿using CsharpBot.Model;
+using Microsoft.Extensions.Options;
+
+namespace CsharpBot.Services
+{
+   
+    public class OpenAIService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IOptions<ChatOptions> _options;
+        public OpenAIService(HttpClient httpClient, IOptions<ChatOptions> options)
+        {
+            _httpClient = httpClient;
+            _options = options;
+        }
+        public async Task<Message> CreateChatCompletion(List<Message> messages)
+        {
+            var request= new { Model=_options.Value.GtpModel,messages=messages.ToArray()};
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _options.Value.ApiKey);
+            var response= await _httpClient.PostAsJsonAsync(_options.Value.ApiUrl, request);    
+            response.EnsureSuccessStatusCode();
+            var chatCompletionResponse= await  response.Content.ReadFromJsonAsync<ChatbotResponse>();
+            return chatCompletionResponse?.choices.First().message;
+        }
+    }
+}
